52d77edf3b213637fa9004db5b1066261674abaa
miguel-kjh <miguelangel.kjh@gmail.com>
2024-12-12 13:06:54 +0000

    add distilation script

---
+++ distilation.py
@@ -0,0 +1,125 @@
+import argparse
+import copy
+import os
+import random
+import pytorch_lightning as pl
+from datasets import load_from_disk
+from distutils.util import strtobool
+import torch
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from lightning.pytorch.loggers import WandbLogger
+import wandb
+import warnings
+import json
+
+warnings.filterwarnings("ignore")
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+
+from utils import prepare_embeddings, SEED, wandb_end, get_timestamp
+from embedding_distilation.EmbeddingDataset import EmbeddingDataset
+from embedding_distilation.EmbeddingAlignmentModel import EmbeddingAlignmentModel
+
+def generate_names_for_wandb_run(args: dict) -> str:
+    large_model = args.large_model.split("/")[-1]
+    small_model = args.small_model.split("/")[-1]
+    dataset_name = args.dataset.split("/")[-1]
+    epochs = args.max_epochs
+    batch_size = args.batch_size
+    context = args.context
+    inner_dim = args.inner_dim
+    data_porcentage = args.data_porcentage
+    time_now = get_timestamp()
+    return f"{large_model}-{small_model}-{dataset_name}-e{epochs}-b{batch_size}-c{context}-id{inner_dim}-dp{data_porcentage}-{time_now}"
+
+def parse_args() -> dict:
+    parser = argparse.ArgumentParser(description="Embedding distillation script")
+    parser.add_argument("--wandb", type=lambda x: bool(strtobool(x)), default=False)
+    parser.add_argument("--context", type=int, default=512, help="Maximum context size for embeddings")
+    parser.add_argument("--batch_size", type=int, default=4, help="Batch size for DataLoader")
+    parser.add_argument("--inner_dim", type=int, default=512, help="Inner dimension for the alignment model")
+    parser.add_argument("--max_epochs", type=int, default=10, help="Maximum number of epochs for training")
+    parser.add_argument("--large_model", type=str, default="EleutherAI/pythia-410m", help="Large model path")
+    parser.add_argument("--small_model", type=str, default="EleutherAI/pythia-70m", help="Small model path")
+    parser.add_argument("--dataset", type=str, default="data/03-combined/tiny", help="Dataset path")
+    parser.add_argument("--output_dir", type=str, default="models/connectors", help="Directory to save the model")
+    parser.add_argument("--project_name", type=str, default="embedding-distilation", help="WandB project name")
+    parser.add_argument("--data_porcentage", type=float, default=0.1, help="Porcentage of data to use")
+    args = parser.parse_args()
+
+    time_now = get_timestamp()
+    dataset_name = args.dataset.split("/")[-1]
+    args.run_name = f"{args.large_model.split('/')[-1]}-{args.small_model.split('/')[-1]}-{dataset_name}-e{args.max_epochs}-b{args.batch_size}-c{args.context}-id{args.inner_dim}-{time_now}"
+    os.makedirs(args.output_dir, exist_ok=True)
+    
+    return args
+
+def setup_wandb(args: dict) -> WandbLogger:
+    wandb.init(project=args.project_name, entity="miguel_kjh", name=args.run_name)
+    wandb_logger = WandbLogger()
+    return wandb_logger
+
+def save_model_connector(model: pl.LightningModule, folder: str, name_model: str = "connector.pth"):
+    connector = model.connector
+    connecto_folder = os.path.join(folder, name_model)
+    os.makedirs(os.path.join(folder, name_model), exist_ok=True)
+    torch.save(connector.state_dict(), os.path.join(connecto_folder, "weights.pth"))
+    with open(os.path.join(connecto_folder, "config.json"), "w") as f:
+        json.dump(connector.get_hyperparameters(), f, indent=4)
+
+
+def main(args: dict):
+    pl.seed_everything(SEED)
+    logger = setup_wandb(args) if args.wandb else None
+    # Load Tokenizer and Models
+    tokenizer           = AutoTokenizer.from_pretrained(args.small_model)
+    tokenizer.pad_token = tokenizer.eos_token
+    model_small         = AutoModelForCausalLM.from_pretrained(args.small_model)
+    model_large         = AutoModelForCausalLM.from_pretrained(args.large_model)
+
+    # Dataset and Embeddings
+    dataset   = load_from_disk(args.dataset)
+    train_sentences = dataset['train']['text']
+    if args.data_porcentage < 1:
+        train_sentences = random.sample(train_sentences, len(train_sentences))
+        train_sentences = train_sentences[:int(len(train_sentences) * args.data_porcentage)]
+    # TODO: improve prepare_embeddings to generate embeddings for all text
+    embeddings_large = prepare_embeddings(tokenizer, model_large, train_sentences, context=args.context)
+    embeddings_small = prepare_embeddings(tokenizer, model_small, train_sentences, context=args.context)
+    val_sentences = dataset['validation']['text']
+    embeddings_large_val = prepare_embeddings(tokenizer, model_large, val_sentences, context=args.context)
+    embeddings_small_val = prepare_embeddings(tokenizer, model_small, val_sentences, context=args.context)
+
+    # Dataset and DataLoader
+    dataset        = EmbeddingDataset(embeddings_large, embeddings_small)
+    dataloader     = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+    dataset_val    = EmbeddingDataset(embeddings_large_val, embeddings_small_val)
+    dataloader_val = DataLoader(dataset_val, batch_size=args.batch_size, shuffle=True, num_workers=4)
+
+    # Initialize Model
+    model = EmbeddingAlignmentModel(
+        large_embedding_dim=embeddings_large.shape[-1],
+        small_embedding_dim=embeddings_small.shape[-1],
+        projected_dim=embeddings_small.shape[-1],
+        inner_dim=args.inner_dim,
+    )
+
+    # Training
+    trainer = pl.Trainer(
+        max_epochs=args.max_epochs,
+        logger=logger,
+    )
+    trainer.fit(model, dataloader, dataloader_val)
+
+    # Save Model only connector
+    save_model_connector(model, args.output_dir, f"connector_{args.run_name}")
+
+    if args.wandb:
+        wandb_end()
+
+# Main Training Script
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
+    
---
+++ utils.py
@@ -0,0 +1,126 @@
+import os
+import warnings
+
+from accelerate import Accelerator, FullyShardedDataParallelPlugin
+from datetime import datetime
+import pandas as pd
+from torch.distributed.fsdp.fully_sharded_data_parallel import FullOptimStateDictConfig, FullStateDictConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+import wandb
+from lightning import seed_everything
+import torch
+
+#WANDB CONFIG
+PROJECT_NAME = "embedding-alignment"
+SEED = 3407
+
+# Folders
+DATA_FOLDER           = "data"
+RAW_DATA_FOLDER       = os.path.join(DATA_FOLDER, "01-raw")
+PROCESS_DATA_FOLDER   = os.path.join(DATA_FOLDER, "02-processed")
+COMBINED_DATA_FOLDER  = os.path.join(DATA_FOLDER, "03-combined")
+
+def get_timestamp():
+    now = datetime.now()
+    return now.strftime("%Y-%m-%d-%H-%M-%S")
+
+def setup_environment(args):
+    warnings.filterwarnings("ignore")
+    if not args.wandb:
+        os.environ["WANDB_DISABLED"] = "true"
+    else:
+        os.environ["WANDB_PROJECT"] = PROJECT_NAME
+        wandb.init(project=PROJECT_NAME, entity="miguel_kjh", name=args.run_name)
+    seed_everything(seed=SEED)
+    torch.backends.cudnn.deterministic = True
+
+def generate_names_for_wandb_run(args):
+    model_name = args.model_name_or_path.split("/")[-1]
+    dataset_name = args.dataset_name.split("/")[-1]
+    epochs = args.num_train_epochs
+    batch_size = args.batch_size
+    lr = args.lr
+    weight_decay = args.weight_decay
+    neftune_noise_alpha = args.neftune_noise_alpha
+    context = args.context
+    name_experiment  = f"{model_name}-{dataset_name}-e{epochs}-b{batch_size}-lr{lr}-wd{weight_decay}-c{context}"
+    name_experiment += f"-r{args.lora_r}-a{args.lora_alpha}-d{args.lora_dropout}" if args.lora else ""
+    name_experiment += f"-nna{neftune_noise_alpha}" if neftune_noise_alpha is not None else ""
+    name_experiment += "-quant" if args.quantization else ""
+    name_experiment += f"-conn-{args.type_connector}" if args.connector else ""
+    name_experiment += f"-{get_timestamp()}"
+    return name_experiment
+
+def create_accelerator():
+    fsdp_plugin = FullyShardedDataParallelPlugin(
+        state_dict_config=FullStateDictConfig(offload_to_cpu=True, rank0_only=False),
+        optim_state_dict_config=FullOptimStateDictConfig(offload_to_cpu=True, rank0_only=False),
+    )
+
+    return Accelerator(fsdp_plugin=fsdp_plugin)
+
+
+def create_model_and_tokenizer(args):
+    if args.quantization:
+        print("### Using Quantization ###")
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit= True,
+            bnb_4bit_quant_type= "nf4",
+            bnb_4bit_compute_dtype= torch.bfloat16,
+            bnb_4bit_use_double_quant= False,
+        )
+        model = AutoModelForCausalLM.from_pretrained(
+            args.model_name_or_path,
+            quantization_config=bnb_config,
+            device_map={"": 0}
+        )
+    else:
+        model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path)
+    accelerator = create_accelerator()
+    model = accelerator.prepare_model(model)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
+    tokenizer.pad_token = tokenizer.eos_token
+    return tokenizer, model
+
+def upload_to_wandb(table_name: str, summaries: list):
+    df_original = pd.DataFrame(summaries)
+    table_name = f"{wandb.run.name}-{table_name}"
+    wandb.log(
+        {
+            table_name: wandb.Table(dataframe=df_original)
+        }
+    )
+
+def wandb_end():
+    wandb.finish()
+
+
+# Preparación de embeddings
+def prepare_embeddings(tokenizer, model, sentences, context=32, stride=16):
+    embeddings = []
+    
+    for sentence in sentences:
+        # Tokenizar la frase completa
+        input_ids = tokenizer(sentence, return_tensors="pt").input_ids.squeeze(0)
+        num_tokens = input_ids.size(0)
+        
+        # Crear ventanas deslizantes
+        windows = [
+            input_ids[i:i + context]
+            for i in range(0, max(1, num_tokens - context + 1), stride)
+        ]
+        
+        # Asegurar que la última ventana siempre se incluya
+        if len(windows) == 0 or windows[-1].size(0) < context:
+            windows.append(input_ids[-context:])
+        
+        # Generar embeddings para cada ventana
+        for window in windows:
+            padded_window = torch.nn.functional.pad(
+                window, (0, context - window.size(0)), value=tokenizer.pad_token_id
+            )  # Rellena si es necesario
+            embedding = model.gpt_neox.embed_in(padded_window.unsqueeze(0)).detach()
+            embeddings.append(embedding.squeeze(0))  # Saca la dimensión del batch
+
+    # Combinar todos los embeddings en un solo tensor
+    return torch.stack(embeddings)
